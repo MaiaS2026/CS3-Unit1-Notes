cb70e02261bf71902dfa8c237cd0e57d2a31d49a
MaiaS2026 <maia.salgado2026@gbwl.org>
2025-09-12 13:40:04 +0000

    pyth notes

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-def main():
-    print("hello world")
+print("Hello world")
 
+# Variables store data
+lucky_num = 26 #int
+my_name = "Maia" #str
+cash = 5.50 #float
+is_raining = True
+is_Sunny = False
 
-if __name__ == "__main__":
-    main()
+# CONCATENATE a string literal + string variable
+greeting = "Howdy, " + my_name
+print(greeting)
+# You can use single quotes instead
+greeting = 'Python is cool, it\'s better than Java'
+print(greeting)
+# You can use triple double quotes for long strings
+long_greeting = """
+                we were both young
+                when i first saw you
+                i close my eyes
+                """
+print(long_greeting)
+# f-strings are FORMATTED strings (like templates)
+f_string = f"My name is {my_name} and my lucky number is {lucky_num}."
+print(f_string)
+
+def helloworld():
+    print("hello world I am a function")
+
+helloworld()
+
+# define a func with arguments
+def multiply(x, y):
+    result = x * y 
+    return result
+
+# call a nonvoid func
+five_times_three = multiply (5, 3)
+print(five_times_three)
+print(multiply(20,6))
