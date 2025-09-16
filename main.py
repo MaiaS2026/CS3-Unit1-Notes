0971a12e2ac55c8d5d6e151ea8691298c2aa7663
MaiaS2026 <maia.salgado2026@gbwl.org>
2025-09-16 19:08:51 +0000

    1.2 notes

--- main.py
+++ main.py
@@ -38,3 +38,41 @@ def multiply(x, y):
 five_times_three = multiply (5, 3)
 print(five_times_three)
 print(multiply(20,6))
+
+# *** LISTS ***
+empty_list = list()
+another_empty_list = []
+
+class_roster = ["Bryce", "Finny", "Jackson", "Kevin", "Maia", "Natalie", "Paige"]
+print(class_roster)
+print(len(class_roster))
+
+# Indexes start at 0
+first_item = class_roster[0]
+print(first_item)
+
+# Update an item in a list, access by index
+class_roster[2] = "Jack"
+print(class_roster)
+
+# Sorting lists
+lottery_nums = [13, 7, 89, 99, 44, 23, 4]
+print(sorted(lottery_nums))
+print(sorted(lottery_nums, reverse=True))
+print(lottery_nums) # sorted () does not modify OG list
+# Sort IN PLACE -> modifies OG list
+lottery_nums.sort()
+print(lottery_nums)
+class_roster.sort(reverse=True)
+print(class_roster)
+
+# List operations
+class_roster.append("Alex")
+class_roster.insert(0, "Zoie")
+class_roster.remove("Zoie")
+class_roster.pop() # Should remove last item
+print(class_roster)
+
+# Check if item exists in a list
+print(13 in lottery_nums)
+print(26 in lottery_nums)
